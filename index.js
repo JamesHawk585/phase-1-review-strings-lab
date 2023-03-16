d9f11ce688656f5a7fa0437120285530e8804217
James Hawk <jph94880@gmail.com>
2023-03-15 23:23:43 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+let currentUser = 'James';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+console.log(excitedWelcomeMessage);
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+console.log(currentUser);
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
